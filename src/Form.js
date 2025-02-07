8bd6439a9e214d44ac6a955343f2613e5aad4d74
Paul-Cavain <paulcavain01@gmail.com>
2025-02-07 12:09:36 +0300

    state management

--- src/Form.js
+++ src/Form.js
@@ -1,9 +1,22 @@
-const Form = () => {
+const Form = ({
+  description,
+  setDescription,
+  quantity,
+  setQuantity,
+  handleSubmit,
+}) => {
   return (
-    <form className="flex flex-row justify-center items-center gap-5 bg-yellow-700 py-4 w-full">
+    <form
+      className="flex flex-row justify-center items-center gap-5 bg-yellow-700 py-4 w-full"
+      onSubmit={handleSubmit}
+    >
       <h3 className="text-lg">What do you need for your 🥰 trip</h3>
 
-      <select className="w-20 h-10 text-xs rounded-md focus:outline-none hover:bg-yellow-700 hover:text-white hover:border-2 hover:border-white transition ease-out duration-500">
+      <select
+        className="w-20 h-10 text-xs rounded-md focus:outline-none hover:bg-yellow-700 hover:text-white hover:border-2 hover:border-white transition ease-out duration-500"
+        value={quantity}
+        onChange={(e) => setQuantity(Number(e.target.value))}
+      >
         {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
           <option value={num} key={num}>
             {num}
@@ -12,9 +25,10 @@ const Form = () => {
       </select>
       <input
         type="text"
-        required
         placeholder="item..."
         className="h-10 w-28 px-1 rounded-md focus:outline-none hover:bg-yellow-700 hover:text-white hover:border-2 hover:border-white transition ease-out duration-500"
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
       />
       <button
         type="button"
--- src/PackingList.js
+++ src/PackingList.js
@@ -4,7 +4,7 @@ const PackingList = ({ itemList }) => {
   return (
     <section className="flex flex-col justify-between items-center py-8 bg-yellow-950 w-full h-[60vh]">
       {/* items */}
-      <div className="grid grid-cols-4 justify-center items-center gap-x-48 gap-y-8 text-white">
+      <div className="grid grid-cols-4 justify-center items-center gap-x-48 gap-y-6 text-white">
         {itemList.map((item, id) => (
           <label
             key={id}
